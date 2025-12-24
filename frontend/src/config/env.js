1726a966d28a7e8eb5d72970f6924476443c8f7f
Anand Singh <anandsinghas9777@gmail.com>
2025-12-25 01:32:41 +0530

    Initial commit

--- frontend/src/config/env.js
+++ frontend/src/config/env.js
@@ -1,2 +1,2 @@
-export const VAULT_ADDRESS = import.meta.env.VITE_VAULT_ADDRESS;
+export const VAULT_ADDRESS ="0xf5a8882Fd8B5fa0091B90b514Db3a3fB6f3099A1";
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
